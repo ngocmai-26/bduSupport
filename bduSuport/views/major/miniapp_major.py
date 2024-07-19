31757e941eb0fdd200cafbd8e3a843295a814eab
thuannt thuannt <thuannt@vnpay.vn>
2024-07-19 13:06:03 +0700

    api to get college exam group from major

--- bduSuport/views/major/miniapp_major.py
+++ bduSuport/views/major/miniapp_major.py
@@ -4,6 +4,8 @@ from rest_framework.decorators import action
 from bduSuport.helpers.response import RestResponse
 from bduSuport.middlewares.miniapp_authentication import MiniAppAuthentication
 from bduSuport.models.evaluation_method import EvaluationMethod
+from bduSuport.models.major import Major
+from bduSuport.serializers.college_exam_group import CollegeExamGroupSerializer
 from bduSuport.serializers.evaluation_method_serializer import EvaluationMethodSerializer
 
 class MiniappMajorView(viewsets.ViewSet):
@@ -17,4 +19,17 @@ class MiniappMajorView(viewsets.ViewSet):
             return RestResponse(data=data, status=status.HTTP_200_OK).response
         except Exception as e:
             print(f"MiniappMajorView.get_evaluation_methods_by_acadmic_major exc={e}")
+            return RestResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR).response
+        
+    @action(methods=["GET"], detail=True, url_path="college-exam-groups")
+    def get_college_exam_groups_by_acadmic_major(self, request, pk):
+        try:
+            try:
+                groups = Major.objects.get(id=pk).college_exam_groups.all()
+                data = CollegeExamGroupSerializer(groups, many=True).data
+                return RestResponse(data=data, status=status.HTTP_200_OK).response
+            except Major.DoesNotExist:
+                return RestResponse(status=status.HTTP_404_NOT_FOUND).response
+        except Exception as e:
+            print(f"MiniappMajorView.get_college_exam_groups_by_acadmic_major exc={e}")
             return RestResponse(status=status.HTTP_500_INTERNAL_SERVER_ERROR).response
