20f97b744fdaf941da73801d0368e6ff6424ba14
thuannt thuannt <thuannt@vnpay.vn>
2024-10-11 17:13:43 +0700

    fix bug

--- bduSuport/middlewares/custom_exception_handler.py
+++ bduSuport/middlewares/custom_exception_handler.py
@@ -7,7 +7,8 @@ from bduSuport.helpers.response import RestResponse
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
 
-    if getattr(response.data.get("detail", None), "code", None) in ["authentication_failed", "not_authenticated"]:
-        return RestResponse(code="invalid_session", status=status.HTTP_401_UNAUTHORIZED).response
+    if response.data is not None:
+        if getattr(response.data.get("detail", None), "code", None) in ["authentication_failed", "not_authenticated"]:
+            return RestResponse(code="invalid_session", status=status.HTTP_401_UNAUTHORIZED).response
 
     return response
