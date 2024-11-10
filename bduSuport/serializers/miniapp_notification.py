ddd19c3c0d70a1470b513f7d7486d853c7a3528e
thuannt thuannt <thuannt@vnpay.vn>
2024-11-10 10:54:11 +0700

    uodate

--- bduSuport/serializers/miniapp_notification.py
+++ bduSuport/serializers/miniapp_notification.py
@@ -1,9 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from ..models.miniapp_notification import MiniappNotification
 
-class MiniappNotificationSerializer(ModelSerializer):
+class MiniappNotificationSerializer(serializers.ModelSerializer):
     class Meta: 
         model = MiniappNotification
         fields = "__all__"
+
+    is_read = serializers.SerializerMethodField()
+
+    def get_is_read(self, obj: MiniappNotification):
+        return obj.read_at is not None
         
         
