8591cc2d49b13fc910b33532c41dd183d28278ee
thuannt thuannt <thuannt@vnpay.vn>
2025-04-03 16:29:18 +0700

    update response

--- bduSuport/serializers/reservation.py
+++ bduSuport/serializers/reservation.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from bduSuport.models.reservation import Reservation
 from bduSuport.serializers.major_serializer import MajorSerializer
+from bduSuport.const.provinces import vietnam_provinces
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,3 +9,7 @@ class ReservationSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     major = MajorSerializer()
+    province_name = serializers.SerializerMethodField()
+
+    def get_province_name(self, obj: Reservation):
+        return vietnam_provinces.get(obj.province, "")
