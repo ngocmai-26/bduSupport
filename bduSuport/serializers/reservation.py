8685941253da417881d6fe890522d250b54d9c09
thuannt thuannt <thuannt@vnpay.vn>
2025-04-03 16:16:20 +0700

    update response

--- bduSuport/serializers/reservation.py
+++ bduSuport/serializers/reservation.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from bduSuport.models.reservation import Reservation
+from bduSuport.serializers.major_serializer import MajorSerializer
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = "__all__"
+
+    major = MajorSerializer()
